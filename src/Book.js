c7ac3dffbf28bf83666a71c6747be35e743d8310
Alireza Ebadi Ghajari <alirezaebg@gmail.com>
2020-08-13 01:11:02 -0700

    Add the value attribute to the select tag in the Book component

--- src/Book.js
+++ src/Book.js
@@ -13,7 +13,7 @@ class Book extends Component {
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                     <div className="book-shelf-changer">
-                        <select onChange={(e) => {
+                        <select value={book.shelf} onChange={(e) => {
                             this.handleChange(book, e.target.value)
                         }}>
                             <option value="move" disabled>Move to...</option>
